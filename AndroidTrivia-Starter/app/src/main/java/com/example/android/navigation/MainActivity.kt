6163886342343c6f62ffb1697daa57482154cec9
JoeyYang <joeyyang@pcpartner.com>
2020-11-02 17:36:11 +0800

    Ch3-2-10 Add the navigation drawer
    文中描述重點:
    1. The navigation drawer is part of the Material Components for Android library, or Material library. 
    
    2.裡面有寫:
    Note: If you use the same ID for the menu item as for the destination Fragment, you don't need to write any code at all to implement the onClick listener! (如果 menu item的ID,與destination fragment(在navigation.xml)的ID是一樣的,就不用再自己寫onClick listener,可以直接使用)
    
    參考網址: https://codelabs.developers.google.com/codelabs/kotlin-android-training-add-navigation/index.html#9

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,28 +19,35 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout  // 3-2-10 Step 5
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout // 3-2-10 Step 5-2
 
         val navController = this.findNavController(R.id.myNavHostFragment)
         // find the navigation controller, myNavHostFragment is in activity_main.xml
         // 3-2-8 step 1, After step 1 ~ step 3, Up button會顯示,且他的popUp作用,就跟 navigation.xml 的設定一模一樣
 
-        NavigationUI.setupActionBarWithNavController(this, navController)
-        // 3-2-8 step 2
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        // 3-2-8 step 2,  3-2-10 Step 5-3
+
+        NavigationUI.setupWithNavController(binding.navView, navController); // 3-2-10 Step 4
     }
 
     // 3-2-8 step 3
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)  // 3-2-10 Step 5-4 Modify
     }
 
     // TODO (01) Create the new TitleFragment
