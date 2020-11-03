fe5ed1302f884a3288519cbcf9af1010d28a395e
JoeyYang <joeyyang@pcpartner.com>
2020-11-03 10:08:18 +0800

    Ch3-3-3 Set up and use the Safe Args plugin
    What I do:
    1. Safe args - NavDirection 的使用
    2. Readme的建立
    
    參考: https://codelabs.developers.google.com/codelabs/kotlin-android-training-start-external-activity/index.html#2

---
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/Readme.java
@@ -0,0 +1,16 @@
+package com.example.android.navigation;
+
+/*
+ Ch3-3-3 重點筆記
+  網址: https://codelabs.developers.google.com/codelabs/kotlin-android-training-start-external-activity/index.html#2
+Safe args - NavDirection (一旦 gradle 加入了safeargs, Safe args plugin就會為每個Fragment建立 NavDirection, 以此例來說,GameFragment 有一個 GameFragmentdirections class,可用此在other fragments來傳遞)
+為何要用Safe args plugin?
+ 之前建議用Bundle,但會遇到二個問題
+  a. Type mismatch errors.
+  b. Missing key errors.
+ 所以在 Android's Navigation Architecture component包函了Safe Args.
+ Safe Args is a Gradle plugin that generates code and classes that help detect errors at compile-time that might not otherwise be surfaced until the app runs.
+ */
+class Readme {
+
+}
