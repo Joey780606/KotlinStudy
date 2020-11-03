a06a4f5527c26d7a77348726401b0645eebf39aa
JoeyYang <joeyyang@pcpartner.com>
2020-11-03 17:13:49 +0800

    Ch3-3-5 Add an implicit intent and a "share" menu item
    What I do:
     1. Introduce the meaning about "Implicit intent"
     2. Use "share" menu.
    
    Refer website:  https://codelabs.developers.google.com/codelabs/kotlin-android-training-start-external-activity/index.html#4
    
    Author: Joey

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/Readme.java
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/Readme.java
@@ -10,11 +10,20 @@ Safe args - NavDirection (一旦 gradle 加入了safeargs, Safe args plugin就�
   b. Missing key errors.
  所以在 Android's Navigation Architecture component包函了Safe Args.
  Safe Args is a Gradle plugin that generates code and classes that help detect errors at compile-time that might not otherwise be surfaced until the app runs.
+ =========================
 
  Ch3-3-4
   網址: https://codelabs.developers.google.com/codelabs/kotlin-android-training-start-external-activity/index.html#3
   1. Use GameFragmentDirections method in the GameFragment to send argument to GameWonFragment.
   2. Replace Fragment classes with NavDirection classes.
+ ==========================
+
+ Ch3-3-5
+  網址: https://codelabs.developers.google.com/codelabs/kotlin-android-training-start-external-activity/index.html#4
+  1. Implicit intent: Android allows you to use intents to navigate to activities that other apps provide.
+   (簡例就是如果想拍照,不用知道那些app支援拍照,若有多個app能handle相同的implicit intent,Android就會顯示chooser讓使用者選擇)
+
+
  */
 class Readme {
 
