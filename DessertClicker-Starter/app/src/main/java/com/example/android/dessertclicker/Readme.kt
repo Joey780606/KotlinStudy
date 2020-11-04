95e94a21c806689e9de7c90a90312031b11a576b
JoeyYang <joeyyang@pcpartner.com>
2020-11-04 10:57:23 +0800

    Ch4-1-5 Explore lifecycle use cases
    What I do:
    1. 按 Back 鍵做 Garbage collect的動作
    2. onRestart() - onCreate 或 onRestart() 會讓app開起來, onCreate()只會出現一次,其他的就是由 onRestart() 來開啟
    3. 此例無程式變化
    
    Reference website: https://codelabs.developers.google.com/codelabs/kotlin-android-training-lifecycles-logging/index.html#4

---
+++ DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/Readme.kt
@@ -0,0 +1,20 @@
+package com.example.android.dessertclicker
+
+/*
+4-1-3
+基本的 lifecycle 介紹和 log
+
+===========
+4-1-4
+  1. 加入 Timber (log用)
+  2. 建立 Application class (Timber需要使用Application class)
+  3. 實作 Timber
+  Reference website: https://codelabs.developers.google.com/codelabs/kotlin-android-training-lifecycles-logging/index.html#3
+
+===========
+4-1-5 (無程式修改)
+  1. 按 Back 鍵做 Garbage collect的動作
+  2. onRestart() - onCreate 或 onRestart() 會讓app開起來, onCreate()只會出現一次,其他的就是由 onRestart() 來開啟
+  3. 此例無程式變化
+  Reference website: https://codelabs.developers.google.com/codelabs/kotlin-android-training-lifecycles-logging/index.html#4
+ */
