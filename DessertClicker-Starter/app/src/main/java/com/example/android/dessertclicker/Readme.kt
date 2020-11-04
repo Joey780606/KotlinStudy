0f3340e0b0f86bab675c004eef965dab7123cae5
JoeyYang <joeyyang@pcpartner.com>
2020-11-04 18:01:30 +0800

    Ch4-2-3 Avoid lifecycle mistakes
    What I do:
    1.只是做 timer 處理的測試,沒有真正的做法,目前只有在 onStart() 做 dessertTimer.startTimer(), onStop() 做 dessertTimer.stopTimer();
    
    2. Step 2-4 有 Processes and threads的link,有空可以看
    
    3. Step 2-10略提到 leak的問題 
    
    Reference website: https://codelabs.developers.google.com/codelabs/kotlin-android-training-complex-lifecycle/index.html#2

--- DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/Readme.kt
+++ DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/Readme.kt
@@ -17,4 +17,11 @@ package com.example.android.dessertclicker
   2. onRestart() - onCreate 或 onRestart() 會讓app開起來, onCreate()只會出現一次,其他的就是由 onRestart() 來開啟
   3. 此例無程式變化
   Reference website: https://codelabs.developers.google.com/codelabs/kotlin-android-training-lifecycles-logging/index.html#4
+
+==========
+4-2-3
+  1.只是做 timer 處理的測試,沒有真正的做法,目前只有在 onStart() 做 dessertTimer.startTimer(), onStop() 做 dessertTimer.stopTimer();
+  2. Step 2-4 有 Processes and threads的link,有空可以看
+  3. Step 2-10略提到 leak的問題
+  Reference website: https://codelabs.developers.google.com/codelabs/kotlin-android-training-complex-lifecycle/index.html#2
  */
