3542391b466b373138e33482a3ddc7ea8d94312d
JoeyYang <joeyyang@pcpartner.com>
2020-11-09 10:26:19 +0800

    Ch5-1-4 Find problems in the starter app
    What I do:
    1.點出專案問題
    2.onSaveInstanceState() callback
    3.介紹App architecture (MVVV- model-view-viewmodel)
    4.UI controller (ViewModel & ViewModelFactory) (介紹重要)
    5.ViewModel的介紹(重要) GameViewModel的使用
    6.ViewModelFactory 實例化(instantiates) ViewModel object.
    
    Reference website: https://codelabs.developers.google.com/codelabs/kotlin-android-training-view-model/#3

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/Readme.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/Readme.kt
@@ -6,4 +6,15 @@ class Readme {
 /*
   Ch5-1-3 Explore the starter code
     只有介紹程式架構,沒有修改程式
+
+  ==================
+  Ch5-1-4 Find problems in the starter app (只有介紹,沒有修改程式）
+  1.點出專案問題
+  2.onSaveInstanceState() callback
+  3.介紹App architecture (MVVV- model-view-viewmodel)
+  4.UI controller (ViewModel & ViewModelFactory) (介紹重要)
+  5.ViewModel的介紹(重要) GameViewModel的使用
+  6.ViewModelFactory 實例化(instantiates) ViewModel object.
+
+  https://codelabs.developers.google.com/codelabs/kotlin-android-training-view-model/#3
  */
