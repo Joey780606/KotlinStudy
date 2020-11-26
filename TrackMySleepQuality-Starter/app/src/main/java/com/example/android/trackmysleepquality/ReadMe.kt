00299811403976f52f5e5113c38466e7c2efd416
JoeyYang <joeyyang@pcpartner.com>
2020-11-26 13:11:50 +0800

    Ch6-1-1 ~ Ch6-1-3
    What I do and note:
    Ch6-1-1 Welcome
     1. Room is a database library that's part of Android Jetpack. Room takes care of many of the chores of setting up and configuring a database, and makes it possible for your app to interact with the database using ordinary function calls. Under the hood, Room is an abstraction layer on top of an SQLite database.
    
     2. 示意圖重要
     Reference website: https://developer.android.com/codelabs/kotlin-android-training-room-database#0
    
    Ch6-1-2 App overview
    APP說明
    
     Reference website: https://developer.android.com/codelabs/kotlin-android-training-room-database#1
    
    Ch6-1-3 Task: Download and inspect the starter app
     1.照做把app copy進來
     2.Step 2 Inspect the startup app 重要,如果 "Android" 該頁看不到相關的資料,可以切到 "Project" 來看,就可以找的到
    
     Reference website: https://developer.android.com/codelabs/kotlin-android-training-room-database#1

---
+++ TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/ReadMe.kt
@@ -0,0 +1,24 @@
+package com.example.android.trackmysleepquality
+
+class ReadMe {
+    /*
+Ch6-1-1 Welcome
+ 1. Room is a database library that's part of Android Jetpack. Room takes care of many of the chores of setting up and configuring a database, and makes it possible for your app to interact with the database using ordinary function calls. Under the hood, Room is an abstraction layer on top of an SQLite database.
+
+ 2. 示意圖重要
+ Reference website: https://developer.android.com/codelabs/kotlin-android-training-room-database#0
+
+============
+Ch6-1-2 App overview
+APP說明
+
+ Reference website: https://developer.android.com/codelabs/kotlin-android-training-room-database#1
+
+============
+Ch6-1-3 Task: Download and inspect the starter app
+ 1.照做把app copy進來
+ 2.Step 2 Inspect the startup app 重要,如果 "Android" 該頁看不到相關的資料,可以切到 "Project" 來看,就可以找的到
+
+ Reference website: https://developer.android.com/codelabs/kotlin-android-training-room-database#1
+     */
+}
