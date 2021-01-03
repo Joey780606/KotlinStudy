5dd7a55624d2c8977a38f2ea34559befc03da405
Joey yang <yty0606@gmail.com>
2021-01-04 06:22:42 +0800

    Ch7-4-1 ~ Ch7-4-3 Get the starter code and inspect the changes to the ap
    Ch 7-4-1 Welcome
    1. 學習對 RecyclerView 的項目進行click listener的動作
    https://developer.android.com/codelabs/kotlin-android-training-interacting-with-items/#0
    
    Ch 7-4-2 App overview
    1. App簡單說明
    https://developer.android.com/codelabs/kotlin-android-training-interacting-with-items/#1
    
    Ch 7-4-3 Task: Get the starter code and inspect the changes to the app
    1. 使用新的starter app,並檢查(inspect)app的改變
    https://developer.android.com/codelabs/kotlin-android-training-interacting-with-items/#2

---
+++ RecyclerViewClickHandler-Starter/app/src/main/java/com/example/android/trackmysleepquality/ReadMe.kt
@@ -0,0 +1,17 @@
+package com.example.android.trackmysleepquality
+
+class ReadMe {
+    /*
+Ch 7-4-1 Welcome
+1. 學習對 RecyclerView 的項目進行click listener的動作
+https://developer.android.com/codelabs/kotlin-android-training-interacting-with-items/#0
+
+Ch 7-4-2 App overview
+1. App簡單說明
+https://developer.android.com/codelabs/kotlin-android-training-interacting-with-items/#1
+
+Ch 7-4-3 Task: Get the starter code and inspect the changes to the app
+1. 使用新的starter app,並檢查(inspect)app的改變
+https://developer.android.com/codelabs/kotlin-android-training-interacting-with-items/#2
+     */
+}
