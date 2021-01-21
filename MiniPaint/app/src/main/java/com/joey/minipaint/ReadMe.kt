125f8ca3911053608a4252e9733ab74254fcab27
JoeyYang <joeyyang@pcpartner.com>
2021-01-21 16:23:05 +0800

    Official draw view testing
    What I do:
    1. Study draw view and final official testing.
    
    Reference website:
    https://developer.android.com/codelabs/advanced-android-kotlin-training-canvas#0
    
    Author: Joey

---
+++ MiniPaint/app/src/main/java/com/joey/minipaint/ReadMe.kt
@@ -0,0 +1,11 @@
+package com.joey.minipaint
+
+class ReadMe {
+    /*
+    For draw view official testing
+
+    1. Reference website:
+      https://developer.android.com/codelabs/advanced-android-kotlin-training-canvas#0
+
+     */
+}
